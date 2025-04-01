b0a6e44cc7180ef04b42ce722d23751983a2bd31
DavenWaay <daven.waay18@gmail.com>
2025-04-01 22:14:18 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import LandingPage from "./pages/LandingPage";
 import FlashCard from "./pages/FlashCard";
+import SignupPage from "./pages/SignupPage"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<FlashCard />} />
+        <Route path="/" element={<SignupPage />} />
         <Route path="/flashcards" element={<FlashCard />} />
       </Routes>
     </Router>
--- src/pages/FlashCard.js
+++ src/pages/FlashCard.js
@@ -24,6 +24,30 @@ const FlashCard = () => {
                     <h3>Definition</h3>
                 </div>
 
+                <div className={styles.btnSection}>
+            {/* Left Side: Track Progress Placeholder */}
+            <span className={styles.trackProgress}>Track Progress</span>
+
+            {/* Center: Previous & Next Buttons */}
+            <div className={styles.navButtons}>
+                <button className={styles.btnPrev}>
+                    <i className='bx bx-chevron-left'></i>
+                </button>
+                <span>1 / 37</span>
+                <button className={styles.btnNext}>
+                    <i className='bx bx-chevron-right'></i>
+                </button>
+            </div>
+
+            {/* Right Side: Additional Controls */}
+            <div className={styles.sideBtn}>
+                <button className={styles.btnPlay}><i className='bx bx-play'></i></button>
+                <button className={styles.btnShuffle}><i className='bx bx-shuffle'></i></button>
+                <button className={styles.btnSettings}><i className='bx bxs-cog'></i></button>
+                <button className={styles.btnFullscreen}><i className='bx bx-fullscreen'></i></button>
+            </div>
+        </div>
+
           </div>
     </div>
 
